8ed477c70ab2118b70af45ca5375aab7f906d8ff
kimgunwoo <guncow0603@gmail.com>
2024-11-10 16:23:16 +0900

    first commit

---
+++ src/main/java/com/hellduo/HellDuoApplication.java
@@ -0,0 +1,13 @@
+package com.hellduo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HellDuoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HellDuoApplication.class, args);
+    }
+
+}
