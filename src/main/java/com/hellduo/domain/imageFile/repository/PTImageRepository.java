b1ab053e81a6bd999fd19b95eb51062d1b13255c
kimgunwoo <guncow0603@gmail.com>
2024-12-06 12:16:53 +0900

    [#44] feat/pt-page - Feat : PT 이미지 업로드 페이지 구현

--- src/main/java/com/hellduo/domain/imageFile/repository/PTImageRepository.java
+++ src/main/java/com/hellduo/domain/imageFile/repository/PTImageRepository.java
@@ -2,6 +2,8 @@ package com.hellduo.domain.imageFile.repository;
 
 import com.hellduo.domain.imageFile.entitiy.PTImage;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface PTImageRepository extends JpaRepository<PTImage,Long> {
 }
