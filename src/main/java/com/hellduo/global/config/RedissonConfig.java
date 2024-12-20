f3506d1d2933459f36252f28931a9fff6371dca3
kimgunwoo <guncow0603@gmail.com>
2024-12-20 15:21:06 +0900

    [#147] refactor/board - Refactor: Redis 캐시와 TTL을 이용한 베스트 게시글 목록 조회 최적화

--- src/main/java/com/hellduo/global/config/RedissonConfig.java
+++ src/main/java/com/hellduo/global/config/RedissonConfig.java
@@ -3,17 +3,18 @@ package com.hellduo.global.config;
 import org.redisson.Redisson;
 import org.redisson.api.RedissonClient;
 import org.redisson.config.Config;
+import org.springframework.cache.CacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
 import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.RedisSerializationContext;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 import java.time.Duration;
+
 
 @Configuration
 public class RedissonConfig {
@@ -26,19 +27,18 @@ public class RedissonConfig {
     }
 
     @Bean
-    public RedisCacheManager redisCacheManager(RedisConnectionFactory redisConnectionFactory) {
+    public CacheManager redisCacheManager(RedisConnectionFactory cf) {
         RedisCacheConfiguration redisCacheConfiguration = RedisCacheConfiguration.defaultCacheConfig()
                 .serializeKeysWith(RedisSerializationContext.SerializationPair.fromSerializer(new StringRedisSerializer()))
-                .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(new GenericJackson2JsonRedisSerializer()))
-                .entryTtl(Duration.ofMinutes(30L)); // 캐시 만료 시간 설정
+                .serializeValuesWith(
+                        RedisSerializationContext.SerializationPair.fromSerializer(
+                                new GenericJackson2JsonRedisSerializer()))
+                .entryTtl(Duration.ofMinutes(30L)); // 캐쉬 저장 시간 30분 설정
 
-        return RedisCacheManager.RedisCacheManagerBuilder.fromConnectionFactory(redisConnectionFactory)
-                .cacheDefaults(redisCacheConfiguration).build();
-    }
-
-    @Bean
-    public RedisConnectionFactory redisConnectionFactory() {
-        // Spring Data Redis에서 사용하는 RedisConnectionFactory 설정
-        return new LettuceConnectionFactory("localhost", 6379); // Redis 서버 주소와 포트 설정
+        return RedisCacheManager
+                .RedisCacheManagerBuilder
+                .fromConnectionFactory(cf)
+                .cacheDefaults(redisCacheConfiguration)
+                .build();
     }
 }
