22b032b2bef103532fefb1439f820c30cd0a788f
kimgunwoo <guncow0603@gmail.com>
2024-12-20 14:19:28 +0900

    [#145] refactor/user - Refactor: Redis 캐시와 TTL을 이용한 베스트 트레이너 목록 최적화

--- src/main/java/com/hellduo/global/config/RedissonConfig.java
+++ src/main/java/com/hellduo/global/config/RedissonConfig.java
@@ -5,6 +5,15 @@ import org.redisson.api.RedissonClient;
 import org.redisson.config.Config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+import java.time.Duration;
 
 @Configuration
 public class RedissonConfig {
@@ -14,5 +23,22 @@ public class RedissonConfig {
         Config config = new Config();
         config.useSingleServer().setAddress("redis://localhost:6379"); // Redis 서버 주소 설정
         return Redisson.create(config);
+    }
+
+    @Bean
+    public RedisCacheManager redisCacheManager(RedisConnectionFactory redisConnectionFactory) {
+        RedisCacheConfiguration redisCacheConfiguration = RedisCacheConfiguration.defaultCacheConfig()
+                .serializeKeysWith(RedisSerializationContext.SerializationPair.fromSerializer(new StringRedisSerializer()))
+                .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(new GenericJackson2JsonRedisSerializer()))
+                .entryTtl(Duration.ofMinutes(30L)); // 캐시 만료 시간 설정
+
+        return RedisCacheManager.RedisCacheManagerBuilder.fromConnectionFactory(redisConnectionFactory)
+                .cacheDefaults(redisCacheConfiguration).build();
+    }
+
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory() {
+        // Spring Data Redis에서 사용하는 RedisConnectionFactory 설정
+        return new LettuceConnectionFactory("localhost", 6379); // Redis 서버 주소와 포트 설정
     }
 }
